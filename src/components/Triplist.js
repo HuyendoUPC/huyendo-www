04565816cec4113916f089cc50c564602446af6e
Matthew Brookes <mbrookes1304@gmail.com>
2016-10-08 14:22:41 +0200

    Allow user to change number of days in destination

--- src/components/Triplist.js
+++ src/components/Triplist.js
@@ -5,7 +5,7 @@ class TripList extends Component {
   render() {
     const { cities } = this.props;
     return (
-        <Table>
+        <table>
         <thead>
         <tr>
         <th className="trips__destination">Destination</th>
@@ -17,12 +17,12 @@ class TripList extends Component {
                                 return (  
                                     <tr>
                                     <td>{ city.name }</td>
-                                    <td><input type="number" min="1" max="30" step="1" value ={ city.days } /></td>
+                                    <td><input type="number" min="1" max="30" step="1" className="trips__numDays"/></td>
                                     </tr>
                                     );                      
                               })}
         </tbody>
-        </Table>
+        </table>
         );
   }
 };
