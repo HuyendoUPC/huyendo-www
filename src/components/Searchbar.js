e62e0c7c83e6170c1b53df7415f39b0fe32aaabc
Oliver Stenbom <oliver@stenbom.eu>
2016-10-08 09:30:30 +0200

    Check for empty seach value

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import Search from 'react-search'
+import Search from 'react-search';
+import { InputGroup } from 'react-bootstrap';
 
 class SearchBar extends Component {
   constructor (props) {
@@ -10,17 +11,19 @@ class SearchBar extends Component {
   getItemsAsync (searchValue, cb) {
     const apiKey = 'wr9ymUz88Kpzs6de6DJ0tOpnatnz5Wrl';
     const url = 'http://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=' + apiKey + '&term=' + searchValue;
-    fetch(url).then((response) => {
-      return response.json();
-    }).then((results) => {
-      if ( results != undefined ) {
-        let items = results.map(function (element) {
-          return {value: element.label}
-        })
-        this.setState({ airports: items })
-        cb(searchValue)
-      }
-    });
+    if (searchValue) {
+      fetch(url).then((response) => {
+        return response.json();
+      }).then((results) => {
+        if ( results != undefined ) {
+          let items = results.map(function (element) {
+            return {value: element.label}
+          })
+          this.setState({ airports: items })
+          cb(searchValue)
+        }
+      });
+    }
   }
 
   respondToChoices (list) {
@@ -29,14 +32,15 @@ class SearchBar extends Component {
 
   render () {
     return (
-      <div>
-        <Search items={this.state.airports}
+      <InputGroup>
+        <Search className="form-control"
+                items={this.state.airports}
                 placeholder="Enter departure city"
                 NotFoundPlaceholder="No matching airports found"
                 multiple={true}
                 onItemsChanged={this.respondToChoices.bind(this)}
                 getItemsAsync={this.getItemsAsync.bind(this)} />
-      </div>
+      </InputGroup>
     )
   }
 }
