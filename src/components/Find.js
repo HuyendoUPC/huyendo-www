acd58bbe8c5cbc198904b0ea4c6b661d666a32be
Matthew Brookes <mbrookes1304@gmail.com>
2016-10-08 14:48:27 +0200

    Add a submission button

--- src/components/Find.js
+++ src/components/Find.js
@@ -1,11 +1,36 @@
 import React, { Component, PropTypes } from 'react';
-import { Button } from 'react-bootstrap';
+import LaddaButton, { XL, SLIDE_UP } from 'react-ladda';
 
 class Find extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: false};
+  }
+
+  toggle() {
+    console.log("hi");
+    this.setState({
+      loading: !this.state.loading,
+      progress: 0.5,
+    });
+    // Submit form
+  }
+
   render() {
     return (
-      <Button bsStyle="primary" bsSize="large">Find Trip</Button>
-    );
+        <LaddaButton
+        loading={this.state.loading}
+        onClick={this.toggle}
+        data-color="#eee"
+        data-size={XL}
+        data-style={SLIDE_UP}
+        data-spinner-size={30}
+        data-spinner-color="#ddd"
+        data-spinner-lines={12}
+        >
+        Find Trip
+        </LaddaButton>
+        );
   }
 };
 
