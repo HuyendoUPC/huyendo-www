4681444f6e2bd51156954a389707e1d936f822f1
Oliver Stenbom <oliver@stenbom.eu>
2016-10-08 02:27:16 +0200

    Initial react/less server commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// main.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require('../stylesheets/main.less');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+    document.getElementById('example')
+    );
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'       
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      { 
+        test: /\.less$/,
+        loader: "style!css!less?strictMath&noIeCompat"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json'] 
+  }
+};
