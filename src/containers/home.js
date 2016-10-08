be691b7b03297249088db96d8fb784744cad4e6d
Matthew Brookes <mbrookes1304@gmail.com>
2016-10-08 03:55:56 +0200

    Attempt to set up application structure

---
+++ src/containers/home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+//import SearchBar from '../components/searchbar.js';
+
+class Home extends React.Component {
+  render () {
+    return (
+        <div> <h1>vnjf</h1> </div>
+    );
+  }
+}
