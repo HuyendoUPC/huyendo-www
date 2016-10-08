48e20ce8b439c52ec01ae5f20693d07b244436da
Oliver Stenbom <oliver@stenbom.eu>
2016-10-08 08:44:10 +0200

    Added checkbox for searchbar

---
+++ src/components/triplist.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+
+class TripList extends Component {
+  render() {
+    return (
+        <h1>Ello again</h1>
+        );
+  }
+};
+
+export default TripList;
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as HomeActions from '../actions';
 
-//import SearchBar from '../components/searchbar.js';
+import { FormGroup, FormControl, Checkbox } from 'react-bootstrap';
+
+//import SearchBar from '../components/searchbar';
+import TripList from '../components/triplist';
 
 class Home extends Component {
+  constructor(props) {
+    super();
+    this.state = { inOut: false };
+    this.toggleInSearch = this.toggleInSearch.bind(this);
+    this.renderInSearch = this.renderInSearch.bind(this);
+  }
+
+  toggleInSearch() {
+    let { inOut } = this.state;
+    console.log('toggle', inOut);
+    this.setState({
+      inOut: !inOut
+    });
+  }
+
+  renderInSearch() {
+    const { inOut } = this.state;
+    if (inOut) {
+      return (
+        <div>
+          <h3>Where do you want to end up?</h3>
+          <FormGroup bsSize="large">
+            <FormControl type="text" placeholder="Search.." />
+          </FormGroup>
+        </div>
+      );
+    }  
+  }
+
   render () {
     return (
-        <h1>Insert Searchbar here</h1>
+        <div className="trips">
+          <div className="trips__search">
+            <h3>Where do you want to fly from?</h3>
+            <FormGroup bsSize="large">
+              <FormControl type="text" placeholder="Search.." />
+            </FormGroup>
+            { this.renderInSearch() }
+            <Checkbox ref="inOut" onChange={ this.toggleInSearch } >I don&#39;t want to end where I started</Checkbox>
+          </div>
+          <div className="trips__list">
+            <TripList />
+          </div>
+        </div>
     );
   }
+};
+
+const mapStateToProps = (state) => {
+  const { date, inCity, outCity, cities } = state;
+  return {
+    date,
+    inCity,
+    outCity,
+    cities
+  };
 }
 
-export default Home;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({HomeActions}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
