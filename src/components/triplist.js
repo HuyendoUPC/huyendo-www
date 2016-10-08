561772f84ab5c87ac1260b81e305cc07723f3783
Oliver Stenbom <oliver@stenbom.eu>
2016-10-08 10:24:01 +0200

    Add find button

--- src/components/triplist.js
+++ src/components/triplist.js
@@ -1,9 +1,23 @@
 import React, { Component, PropTypes } from 'react';
+import { Table } from 'react-bootstrap';
 
 class TripList extends Component {
   render() {
     return (
-        <h1>Ello again</h1>
+        <Table>
+          <thead>
+            <tr>
+              <th>Destination</th>
+              <th>Days Spent</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Hong Kong</td>
+              <td><input type="number" min="1" max="30" step="1" value ="3" /></td>
+            </tr>
+          </tbody>
+        </Table>
         );
   }
 };
