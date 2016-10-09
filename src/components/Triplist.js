5085582c309a8b22323edd6c44484b208a0d37aa
Oliver Stenbom <oliver@stenbom.eu>
2016-10-09 05:20:27 +0200

    Trip Stops to panel

--- src/components/Triplist.js
+++ src/components/Triplist.js
@@ -1,29 +1,36 @@
 import React, { Component, PropTypes } from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Panel } from 'react-bootstrap';
+import _ from 'underscore';
 
 class TripList extends Component {
   render() {
     const { cities } = this.props;
-    return (
-        <table>
-        <thead>
-        <tr>
-        <th className="trips__destination">Destination</th>
-        <th>Days Spent</th>
-        </tr>
-        </thead>
-        <tbody>
-        {cities.map((city) => {
-                                return (  
-                                    <tr>
-                                    <td>{ city.name }</td>
-                                    <td><input type="number" min="1" max="30" step="1" className="trips__numDays"/></td>
-                                    </tr>
-                                    );                      
-                              })}
-        </tbody>
-        </table>
-        );
+    if (!_.isEmpty(cities)) {
+      return (
+          <Panel className="trip-results__panel" header="Trip Stops">
+          <Table>
+          <thead>
+          <tr>
+          <th className="trips__destination">Stop</th>
+          <th>Days Spent</th>
+          </tr>
+          </thead>
+          <tbody>
+          {cities.map((city) => {
+              return (  
+                  <tr>
+                  <td>{ city.name }</td>
+                  <td><input type="number" min="1" max="30" step="1" className="trips__numDays"/></td>
+                  </tr>
+                  );                      
+            })}
+          </tbody>
+          </Table>
+          </Panel>
+          );
+    } else {
+      return (<div/>);
+    }
   }
 };
 
